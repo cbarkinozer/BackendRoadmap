46a3c7cec9f0f046d44c062375f7075bac828a0e
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-27 14:37:30 +0300

    Update index.js

--- 2_BasicFrontendKnowledge/javascripttodolist/index.js
+++ 2_BasicFrontendKnowledge/javascripttodolist/index.js
@@ -1,16 +1,36 @@
-let list = document.getElementById("list");
-let text = document.getElementById("text");
 
+function newElement() {
+  const exit = document.createElement("span");
+    exit.innerText="x"
+    const li = document.createElement("li")
+    const taskValue = document.getElementById("task").value
+    const valueTag = document.createElement("span");
+    valueTag.innerText =  (taskValue === "" || taskValue === null ) ? null : taskValue
+    li.append(valueTag)
+    const taskList = document.getElementById("list")
+    li.appendChild(exit)
+    li.onclick = () => done(li)
+    exit.onclick= () => exit.parentElement.remove();
 
-text.addEventListener("click",newElement(){
-  // Input ile gelen veriyi "li" içerisine metin olarak giriyoruz.
-  list.textContent=text.value; 
-  // appendChild ile oluşturduğumuz elementi "liste" isimli değişkenimizin içerisine ekliyoruz.
-  list.appendChild(li);     
-  // Veri inputu içerisindeki metni siliyoruz.
-  text.value = "";
+    if(taskValue !== "" && taskValue !== null){
+        document.querySelector("#liveToast").className="toast show"
+        setTimeout(()=> document.querySelector(
+            "#liveToast").className="toast",3000)
+        taskList.appendChild(li);
+    }else{
+        const warning = document.querySelector("#liveToastNull");
+        warning.className="toast error show"
+        setTimeout(()=> warning.className="toast error hide",2000)
+    }
+
+    document.getElementById("task").value=""
+
 }
-function deleteElement(){
-  list.style.color  = "blue";
+
+function deleteElement(exit){
+  console.log(exit)
+  exit.parentElement.remove()
 }
-function done(){}
+function done(element){
+element.lastChild.onclick= () => element.lastChild.parentElement.remove()
+}
