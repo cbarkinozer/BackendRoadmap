b2f7d5365ff01f7380fa3d134b3fb38f2dc00f73
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-25 10:22:20 +0300

    Update app.js

--- 2_BasicFrontendKnowledge/asiankitchen/app.js
+++ 2_BasicFrontendKnowledge/asiankitchen/app.js
@@ -19,7 +19,7 @@ const menu = [
   },
   {
     id: 3,
-    title: "Bibimbap",
+    title: "DWELL Bibimap",
     category: "Korea",
     price: 8.99,
     img:
@@ -81,3 +81,105 @@ const menu = [
     desc: `Red bean paste dessert, serving with honey.`,
   },
 ];
+
+// Part above is given for the homework
+//We will make the above part work as in HTML file
+
+/* ------------------------------To Do---------------------------------------------
+Define website's content in an array of objects inside menu.
+There will be Id, title, category, price, img ve desc parts.
+Buttons will be at javascript file too
+You need to use map reduce methods
+Creating a new list by changing array's elements using map
+-------------------------------------------
+Define menu elements in a class named action-center, select this with querryselector and define as section
+Button elements are also in a container called btn-container, select this with querryselector and define as buttonConteiner
+*/
+let section = document.querySelector(".section-center")
+let butonContainerim = document.querySelector(".btn-container")
+
+//Defining buttons
+//It will write the given button name to the html element's id and body
+function createButton(buttonName) {
+  let btn = `<button class="btn btn-outline-dark btn-item" id="${buttonName}">${buttonName}</button>`
+  return btn
+}
+
+// Adding buttons
+function addButton() {
+  // If we run crateButton inside the btn container with the help of the container we created above, we will have buttons named below.
+  butonContainerim.innerHTML = createButton("All") + " " + createButton("Korea") + " " + createButton("Japan") + " " + createButton("China")
+  //We are defining elements that will be selected when pressed on them
+  //We defined addFoods functions a little below
+  document.getElementById('All').addEventListener('click',addFoodsAll)
+  document.getElementById('Korea').addEventListener('click',addFoodsKorea)//When pressed it will get Korea named button and will run addFoodsKorea method
+  document.getElementById('Japan').addEventListener('click',addFoodsJapan)
+  document.getElementById('China').addEventListener('click',addFoodsChina)
+}
+
+
+
+// Defining foods
+// And it defines a fod that its title, id ,img ,price,description's are defined at above const menu
+function foodCreator(name) {
+  let item = `<div class="menu-items col-lg-6 col-sm-12">
+  <img src="${name.img}" alt="${name.title}" class="photo">
+  <div class="menu-info">
+    <div class="menu-title">
+      <h4>${name.title}</h4>
+      <h4 class="price">${name.price}</h4>
+    </div>
+    <div class="menu-text">
+      ${name.desc}
+    </div>
+  </div>
+</div>`
+  return item
+}
+
+
+
+// Adding all menu
+// Foods that are coming out of the creatFood function are looped here
+// Sends each element to foodCreator and processes
+//This is for no filter
+
+function addFoodsAll() {
+  section.innerHTML = ""
+  menu.forEach(element => {
+    section.innerHTML += (" " + foodCreator(element))
+  });
+}
+
+// Adding Korea Menu
+//Filter for the category Korea and render  every element that is coming from process with foodCreator by sending it to inner html.
+function addFoodsKorea() {
+  section.innerHTML = ""
+  const koreanMenu = menu.filter(x => x.category == "Korea")
+  koreanMenu.forEach(element => {
+    section.innerHTML += (" " + foodCreator(element))
+  });
+}
+
+// Adding Japan Menu
+function addFoodsJapan() {
+  section.innerHTML = ""
+  const koreanMenu = menu.filter(x => x.category == "Japan")
+  koreanMenu.forEach(element => {
+    section.innerHTML += (" " + foodCreator(element))
+  });
+}
+
+// Adding Chinese Menu
+function addFoodsChina() {
+  section.innerHTML = ""
+  const koreanMenu = menu.filter(x => x.category == "China")
+  koreanMenu.forEach(element => {
+    section.innerHTML += (" " + foodCreator(element))
+  });
+}
+
+//Only buttons come
+window.addEventListener('load',addButton)
+//Do not show all elements at the beginning
+window.addEventListener('load',addFoodsAll)
