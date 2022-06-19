9f02522ccd828df1d321b7f22bc76b9737eb88c5
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-06-19 21:44:54 +0300

    Add files via upload

---
+++ 2_BasicFrontendKnowledge/vueExample/vueJS.vue
@@ -0,0 +1,20 @@
+import * as Vue from "https://cdn.skypack.dev/vue";
+
+new Vue({
+  el: '#app',
+  data: {
+    name: 'Max',
+		elements: []
+  },
+  methods: {
+    changeName: function() {
+      this.name = 'Anna';
+    },
+		addElement: function(){
+			this.elements.push(this.elements.length + 1);
+		},
+		getColor: function(number){
+		return number %2==0?'green':'red';
+	}
+  }
+});
